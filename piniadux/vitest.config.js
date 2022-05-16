8fd485379c9a9e82a7aec076885d7095730929d3
YeanSe <1730214056@qq.com>
2022-05-16 11:37:23 +0800

    cli:改用vitest，增加test脚本

--- piniadux/vitest.config.js
+++ piniadux/vitest.config.js
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: 'happy-dom',
